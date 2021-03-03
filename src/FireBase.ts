5481afab975666fdf4de7f43a57ade7a6e7776c6
Noiri <suleipenn@gmail.com>
2021-03-04 07:55:27 +0900

    Set up FireBase.

---
+++ src/FireBase.ts
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const { env } = process;
+
+const firebaseConfig = {
+    apiKey: env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: env.REACT_APP_FIREBASE_APP_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
+export const db = firebase.firestore();
