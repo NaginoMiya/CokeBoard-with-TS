0580fee611e7e50b5160dedffff7fc5adba5fb7b
Noiri <suleipenn@gmail.com>
2021-03-03 09:47:31 +0900

    MainPageを配置.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import {FC} from "react";
-import logo from "./logo.svg";
+import { FC } from "react";
 import "./App.css";
 
-const App:FC = () => (
+import MainPage from "./PresentationalComponents/Pages/MainPage";
+
+const App: FC = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MainPage isLoading={false} />
     </div>
-  )
+);
 
 export default App;
