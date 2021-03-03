61dbf959b1e780370d7a96ddf50b82b0a0714584
Noiri <suleipenn@gmail.com>
2021-03-03 09:47:53 +0900

    DnD Providerを設置.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <DndProvider backend={HTML5Backend}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </DndProvider>,
+    document.getElementById("root")
 );
