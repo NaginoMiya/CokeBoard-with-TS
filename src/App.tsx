a9dc1f7c6c2b676938eaeb5a02d3e7e02f18f404
Noiri <suleipenn@gmail.com>
2021-03-07 23:23:13 +0900

    Test FireBase CloudFireStore

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,22 @@ import "./App.css";
 
 import MainPage from "./PresentationalComponents/Pages/MainPage";
 
-const App: FC = () => (
-    <div className="App">
-        <MainPage isLoading={false} />
-    </div>
-);
+import { db } from "./FireBase";
+
+const App: FC = () => {
+    // FireBaseのテスト
+    const tmp = async () => {
+        const docRef = db.collection("ts-test").doc("test-doc");
+        console.log((await docRef.get()).data());
+    };
+
+    tmp().catch((err) => console.log(err));
+
+    return (
+        <div className="App">
+            <MainPage isLoading={false} />
+        </div>
+    );
+};
 
 export default App;
