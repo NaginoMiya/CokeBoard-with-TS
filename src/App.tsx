4175ef1b589e77b3296f72bc900f42e72991ffda
Noiri <suleipenn@gmail.com>
2021-03-07 23:30:22 +0900

    remove console.log

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,10 @@ import "./App.css";
 
 import MainPage from "./PresentationalComponents/Pages/MainPage";
 
-import { db } from "./FireBase";
-
-const App: FC = () => {
-    // FireBaseのテスト
-    const tmp = async () => {
-        const docRef = db.collection("ts-test").doc("test-doc");
-        console.log((await docRef.get()).data());
-    };
-
-    tmp().catch((err) => console.log(err));
-
-    return (
-        <div className="App">
-            <MainPage isLoading={false} />
-        </div>
-    );
-};
+const App: FC = () => (
+    <div className="App">
+        <MainPage isLoading={false} />
+    </div>
+);
 
 export default App;
