3ffab57672e23e88d9d940146a9172ab776b315f
Noiri <suleipenn@gmail.com>
2021-03-09 07:31:43 +0900

    firebase initを一番最初に実行するように変更.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { FC } from "react";
+import firebase from "firebase/app";
+import firebaseConfig from "./firebaseConfig";
+
 import "./App.css";
 
 import MainPage from "./PresentationalComponents/Pages/MainPage";
 
+firebase.initializeApp(firebaseConfig);
+
 const App: FC = () => (
     <div className="App">
+        {process.env.REACT_APP_FIREBASE_API_KEY}
         <MainPage isLoading={false} />
     </div>
 );
