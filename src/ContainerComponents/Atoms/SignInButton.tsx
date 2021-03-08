be2896d70f6247272559e7a2ae3f990cfa4bb678
Noiri <suleipenn@gmail.com>
2021-03-08 18:27:37 +0900

    recoilでuidを使用するためのテスト.

--- src/ContainerComponents/Atoms/SignInButton.tsx
+++ src/ContainerComponents/Atoms/SignInButton.tsx
@@ -1,15 +1,24 @@
 import { FC, useEffect } from "react";
 import firebase from "firebase";
+import { useRecoilState } from "recoil";
 import EnhancedSignInButton from "../../PresentationalComponents/Atoms/SignInButton";
 
-const SignInButton: FC = () => {
-    useEffect(() => {
-        firebase.auth().onAuthStateChanged((user) => {
-            console.log(user!.uid);
-        });
-    }, []);
+import userState from "../../Recoil/user";
 
-    return <EnhancedSignInButton />;
+const SignInButton: FC = () => {
+    const [user, setUser] = useRecoilState(userState);
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((usr) => {
+            setUser(usr === null ? "" : usr.uid);
+        });
+    }, [setUser]);
+
+    return (
+        <>
+            <EnhancedSignInButton />
+            {user}
+        </>
+    );
 };
 
 export default SignInButton;
