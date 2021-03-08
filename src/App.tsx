d0e85d0a9fa5fdcf52cae9991d54c900b87770a7
Noiri <suleipenn@gmail.com>
2021-03-08 17:40:33 +0900

    firebase initの呼び出しを追加. & authのテスト用にsignInButtonを配置.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { FC } from "react";
+import firebase from "firebase/app";
+import firebaseConfig from "./firebaseConfig";
+
 import "./App.css";
 
 import MainPage from "./PresentationalComponents/Pages/MainPage";
+
+firebase.initializeApp(firebaseConfig);
 
 const App: FC = () => (
     <div className="App">
