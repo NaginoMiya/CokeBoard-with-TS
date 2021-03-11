efaa3b60ea8427ac1cfa3faa0396bc1f0f086bb1
Noiri <suleipenn@gmail.com>
2021-03-11 14:36:05 +0900

    recoil root を削除.

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { RecoilRoot } from "recoil";
+// import { RecoilRoot } from "recoil";
+import { BrowserRouter } from "react-router-dom";
+import firebase from "firebase/app";
+import FirebaseApp from "./FirebaseApp";
+import firebaseConfig from "./firebaseConfig";
+
 import App from "./App";
 
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
-    <RecoilRoot>
-        <DndProvider backend={HTML5Backend}>
-            <React.StrictMode>
-                <App />
-            </React.StrictMode>
-        </DndProvider>
-    </RecoilRoot>,
+    <React.StrictMode>
+        <BrowserRouter>
+            <FirebaseApp>
+                <DndProvider backend={HTML5Backend}>
+                    <App />
+                </DndProvider>
+            </FirebaseApp>
+        </BrowserRouter>
+    </React.StrictMode>,
     document.getElementById("root")
 );
