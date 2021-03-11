e1d5b55be648fcdeee3c1009f63c337025e79c10
Noiri <suleipenn@gmail.com>
2021-03-11 18:25:43 +0900

    Auth完了後にアプリ内リダイレクトするように変更.

--- src/PresentationalComponents/Atoms/SignInButton.tsx
+++ src/PresentationalComponents/Atoms/SignInButton.tsx
@@ -1,25 +1,36 @@
 import { FC } from "react";
+import { useNavigate } from "react-router";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import firebase from "firebase";
-
-// Configure FirebaseUI.
-const uiConfig = {
-    signInSuccessUrl: "App",
-    signInFlow: "redirect",
-    signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    ],
-};
 
 type Props = {
     auth: firebase.auth.Auth | null;
 };
 
-const SignInButton: FC<Props> = ({ auth }) => (
-    <div className="flex-box">
-        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
-    </div>
-);
+const SignInButton: FC<Props> = ({ auth }) => {
+    const navigate = useNavigate();
+
+    // Configure FirebaseUI.
+    const uiConfig = {
+        signInFlow: "redirect",
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        ],
+        callbacks: {
+            signInSuccessWithAuthResult: () => {
+                navigate("/App", { replace: true });
+
+                return false;
+            },
+        },
+    };
+
+    return (
+        <div className="flex-box">
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+        </div>
+    );
+};
 
 export default SignInButton;
