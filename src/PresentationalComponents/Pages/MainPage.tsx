a2da8ec7c5cffc68e278838312d43807648d2a48
Noiri <suleipenn@gmail.com>
2021-03-11 17:11:56 +0900

    必要なくなったのでsingn関係のボタンを削除.

--- src/PresentationalComponents/Pages/MainPage.tsx
+++ src/PresentationalComponents/Pages/MainPage.tsx
@@ -1,7 +1,5 @@
-import { FC, useContext } from "react";
+import { FC } from "react";
 import Grid from "@material-ui/core/Grid";
-import { Navigate } from "react-router";
-import UserContext from "../../Contexts/UserContextValue";
 
 import SideBar from "../Organisms/SideBar";
 import Board from "../Organisms/Board";
@@ -12,24 +10,16 @@ type Props = {
     isLoading: boolean;
 };
 
-const MainPage: FC<Props> = ({ isLoading = false }) => {
-    const { user } = useContext(UserContext);
-
-    return user === null ? (
-        <div>
-            <Navigate to="/" replace />
-        </div>
-    ) : (
-        <Grid container className="MainPage" spacing={0}>
-            <Grid item xs={2} className="MenuBar">
-                <div>{isLoading ? <div>Loading</div> : <SideBar />}</div>
-            </Grid>
-            <Grid item xs={10} className="MemoScreen" spacing={0}>
-                <h1>BoardName</h1>
-                <Board />
-            </Grid>
+const MainPage: FC<Props> = ({ isLoading = false }) => (
+    <Grid container className="MainPage" spacing={0}>
+        <Grid item xs={2} className="MenuBar">
+            <div>{isLoading ? <div>Loading</div> : <SideBar />}</div>
         </Grid>
-    );
-};
+        <Grid item xs={10} className="MemoScreen" spacing={0}>
+            <h1>BoardName</h1>
+            <Board />
+        </Grid>
+    </Grid>
+);
 
 export default MainPage;
