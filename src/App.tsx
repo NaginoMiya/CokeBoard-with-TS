fac43842c8f1de588c2f8a6a9f66ce6bac5624ee
Noiri <suleipenn@gmail.com>
2021-03-11 17:08:37 +0900

    認証済みのときroot->App, そうでないときApp->rootにリダイレクトするように設定.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,41 @@
-import { FC } from "react";
-import { Route, Routes } from "react-router";
+import { FC, useContext } from "react";
+import { Route, Routes, Navigate } from "react-router";
 
 import "./App.css";
 
+import UserContext from "./Contexts/UserContextValue";
 import MainPage from "./PresentationalComponents/Pages/MainPage";
-import SignIn from "./ContainerComponents/Atoms/SignInButton";
-import SignOut from "./ContainerComponents/Atoms/SignOutButton";
+import LandingPage from "./PresentationalComponents/Pages/LandingPage";
 
-const App: FC = () => (
-    <div className="App">
-        <Routes>
-            <Route
-                path="/"
-                element={
-                    <>
-                        <SignIn />
-                        <SignOut />
-                    </>
-                }
-            />
-            <Route path="/App" element={<MainPage isLoading={false} />} />
-        </Routes>
-    </div>
-);
+const App: FC = () => {
+    const { user } = useContext(UserContext);
+
+    return (
+        <div className="App">
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        user === null ? (
+                            <LandingPage />
+                        ) : (
+                            <Navigate to="/App" replace />
+                        )
+                    }
+                />
+                <Route
+                    path="/App"
+                    element={
+                        user === null ? (
+                            <Navigate to="/" replace />
+                        ) : (
+                            <MainPage isLoading={false} />
+                        )
+                    }
+                />
+            </Routes>
+        </div>
+    );
+};
 
 export default App;
